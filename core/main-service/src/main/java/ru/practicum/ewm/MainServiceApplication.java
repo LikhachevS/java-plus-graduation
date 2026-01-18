356d47a38ac08dad67d5a6017be003cb03e3e1c0
Sergey <Lihachev.S.71y@yandex.ru>
2026-01-18 13:15:33 +0300

    Коммит 9. корректировки для постем тестов. Жобавил аннотацию @LoadBalanced к RestTemplate.

--- core/main-service/src/main/java/ru/practicum/ewm/MainServiceApplication.java
+++ core/main-service/src/main/java/ru/practicum/ewm/MainServiceApplication.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class MainServiceApplication {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
