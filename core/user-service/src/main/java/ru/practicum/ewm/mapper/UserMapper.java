b34263477266f7354ee2688ced3d8d4cc64c1708
Sergey <Lihachev.S.71y@yandex.ru>
2026-01-22 17:27:15 +0300

    Коммит 11. Вынес User Service в отдельный модуль, сделал отдельную БД. Постман тесты User Service работают локально корректно.

--- core/user-service/src/main/java/ru/practicum/ewm/mapper/UserMapper.java
+++ core/user-service/src/main/java/ru/practicum/ewm/mapper/UserMapper.java
@@ -1,11 +1,11 @@
-package ru.practicum.ewm.user.mapper;
+package ru.practicum.ewm.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import ru.practicum.ewm.user.dto.NewUserRequest;
-import ru.practicum.ewm.user.dto.UserDto;
-import ru.practicum.ewm.user.dto.UserShortDto;
-import ru.practicum.ewm.user.model.User;
+import ru.practicum.ewm.dto.NewUserRequest;
+import ru.practicum.ewm.dto.UserDto;
+import ru.practicum.ewm.dto.UserShortDto;
+import ru.practicum.ewm.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
